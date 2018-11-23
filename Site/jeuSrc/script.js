780518a5824a760882109dc8310a67431f25c51e
Mehdi Saber <msaber@iut.univ-paris8.fr>
2018-11-23 14:36:01 +0100

    Debut de l'architecture MVC de JS

--- Site/jeuSrc/script.js
+++ Site/jeuSrc/script.js
@@ -7,6 +7,7 @@ var myGameArea = {
 		this.canvas.width = 720;
 		this.canvas.height = 480;
 		this.context = this.canvas.getContext("2d");
+		/*
 		this.context.font = "50px Montserrat";
 		this.context.fillStyle = "#ffdf23";
 		this.context.fillText("ShakDonalds Le Jeu", this.canvas.width/6, this.canvas.height/2);
@@ -14,6 +15,7 @@ var myGameArea = {
 		this.context.fillStyle = "#ffdf23";
 		this.context.fillText("Coming soon, blyat", this.canvas.width/2.75, this.canvas.height/1.5);
 		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+		*/
 		}
 }
 
