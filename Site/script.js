087a0c46b8e7af3b601d2b69e04d2d8309b8bc56
Mehdi Saber <msaber@iut.univ-paris8.fr>
2018-11-23 09:12:04 +0100

    Début de modification du canvas et du JS

--- Site/script.js
+++ Site/script.js
@@ -2,14 +2,17 @@ function startGame() {
 	myGameArea.start();
 }
 var myGameArea = {
-	canvas: document.createElement("canvas"),
+	canvas: document.getElementById("canvasJeu"),
 	start : function() {
 		this.canvas.width = 720;
 		this.canvas.height = 480;
 		this.context = this.canvas.getContext("2d");
 		this.context.font = "50px Montserrat";
 		this.context.fillStyle = "#ffdf23";
-		this.context.fillText("ShakDonalds", this.canvas.height/2.25, this.canvas.height/2);
+		this.context.fillText("ShakDonalds Le Jeu", this.canvas.width/6, this.canvas.height/2);
+		this.context.font = "20px Montserrat";
+		this.context.fillStyle = "#ffdf23";
+		this.context.fillText("Coming soon, blyat", this.canvas.width/2.75, this.canvas.height/1.5);
 		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 		}
 }
