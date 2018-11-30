f69ef89c6390a929867bf41757ba0905dfff5f17
Mehdi Saber <msaber@iut.univ-paris8.fr>
2018-11-30 12:59:37 +0100

    Retrait du bouton et ajout d'un bouton en JS pour exécuter le jeu.

--- Site/jeuSrc/script.js
+++ Site/jeuSrc/script.js
@@ -1,10 +1,24 @@
 //Initialisation des variables
 var joueur;
-var score;
+
+var rect = {
+	x:150,
+	y:350,
+	width:400,
+	heigth:100
+};
+
+function Joueur(nom) {
+	this.nom = nom;
+	this.score = 0;
+	this.argent = 100;
+	this.niveau = 1;
+	this.xp = 0;
+
+}
 
 //NOTE A SOI MEME: mettre les objects dans la meme classe.
 
-//joueur =  new Joueur("bonjour");
 
 //Main du canvas
 function startGame() {
@@ -24,6 +38,19 @@ var myGameArea = {
 		this.context.font = "50px Montserrat";
 		this.context.fillStyle = "#ffdf23";
 		this.context.fillText("ShakDonalds Le Jeu", this.canvas.width/6, this.canvas.height/2);
+
+		this.context.beginPath();
+		this.context.rect(150,350,400,100);
+		this.context.fillStyle = '#ffdf23';
+		this.context.fill();
+		this.context.lineWidth = 2;
+		this.context.strokeStyle = '#000000'; 
+		this.context.stroke();
+		this.context.closePath();
+
+		this.context.font = "20px Montserrat";
+		this.context.fillStyle = "#000000";
+		this.context.fillText("Cliquer pour commencer", 225, 400);
 		/*
 		this.context.font = "20px Montserrat";
 		this.context.fillStyle = "#ffdf23";
@@ -34,16 +61,19 @@ var myGameArea = {
 	clear : function() {
 		this.context.clearRect(0,0,this.canvas.width, this.canvas.height);
 	}
-}
-
-function Joueur(nom) {
-	this.nom = nom;
-	this.score = 0;
-	this.argent = 100;
-	this.niveau = 1;
-	this.xp = 0;
 
 }
+
+myGameArea.canvas.addEventListener('click', function(evt) {
+	var mousePos = getMousePos(myGameArea.canvas, evt);
+    
+	if (isInside(mousePos,rect)) {
+		testMain();
+    }else{
+        alert('Clique dans le rectangle jaune, espèce de gros débile');
+    }	
+}, false);
+
 
 
 //Main du jeu
@@ -53,10 +83,27 @@ function testMain() {
 	myGameArea.context.font = "30px Montserrat";
 	myGameArea.context.fillStyle = "#ffdf23";
 	myGameArea.context.fillText("Nom : " + joueur.nom, myGameArea.canvas.width/4, myGameArea.canvas.height/5);
-	
+	myGameArea.context.fillText("Score : " + joueur.score, myGameArea.canvas.width/4, myGameArea.canvas.height/4);
+	myGameArea.context.fillText("Argent : " + joueur.argent, myGameArea.canvas.width/4, myGameArea.canvas.height/3);
+	myGameArea.context.fillText("Niveau : " + joueur.niveau, myGameArea.canvas.width/4, myGameArea.canvas.height/2);
 }
 
 
+
+function getMousePos(canvas, event) {
+	var rect = canvas.getBoundingClientRect();
+	return {
+		x: event.clientX - rect.left,
+		y: event.clientY - rect.top
+	};
+}
+
+function isInside(pos, rect){
+	return pos.x > rect.x && pos.x < rect.x+rect.width && pos.y < rect.y+rect.heigth && pos.y > rect.y
+}
+
+
+//Drag et drop images
 function allowDrop(ev) {
     ev.preventDefault();
 }
